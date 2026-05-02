0f4bf794f1797da3ab6f29345e99f59e3f8d930c
CIITSHSDTP <ciitshsdtp30@ciit.edu.ph>
2026-05-02 17:02:41 +0800

    week 2 db management

--- apps/CIIT Project DB.sql
+++ apps/CIIT Project DB.sql
@@ -1,12 +1,147 @@
-select * from employee e ;
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ciit project db
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
 
-\\select id,last_name,first_name from employee e ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-\\insert into employee (email, department_id, last_name, first_name, birth_date, date_hired , created_date)
-values ('mahalkong@pilipinas.com', 3, 'BAYANI', 'MAC', '2025-01-01', '2026-12-01', '2026-12-01');
+--
+-- Table structure for table `department`
+--
 
-\\update employee set email='sprikitik@magic.com' where id=3;
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` text DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-\\select id, last_name , first_name from employee e where e.department_id = 1;
+--
+-- Dumping data for table `department`
+--
 
-delete from employee where id=3;
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES (1,'IT','Information Technology','IT'),(2,'FIN','Finance','Financial Transaction'),(3,'EXEC','Executive','Executive Sections');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `date_hired` date NOT NULL,
+  `salary` decimal(10,0) DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `employee_unique` (`email`),
+  KEY `employee_department_FK` (`department_id`),
+  CONSTRAINT `employee_department_FK` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'sadninja09@gmail.com',1,'ROMERO','SAMMYR','M','1991-03-04','2014-05-04',30000,'2026-05-02 03:11:22',NULL),(4,'pilipinas@gmail.com',3,'ROMERO','SEI ROUA','R','2023-11-14','2023-12-01',1000000,'2026-05-01 16:00:00',NULL),(8,'juandelacruz@makabayan.com',2,'DELA CRUZ','JUAN','M','2026-05-02','2026-05-02',1,'2026-05-01 16:00:00',NULL),(9,'abrakadabra@magic.com',3,'DIZON','MICHAEL','S','2024-01-03','2026-12-01',50,'2026-05-01 16:00:00',NULL),(10,'unclejohn@food.com',2,'JOHN','UNCLE',NULL,'2023-01-31','2025-01-31',123000,'2026-05-01 16:00:00',NULL);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee_project`
+--
+
+DROP TABLE IF EXISTS `employee_project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee_project` (
+  `project_id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  KEY `employee_project_project_FK` (`project_id`),
+  KEY `employee_project_employee_FK` (`employee_id`),
+  CONSTRAINT `employee_project_employee_FK` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`),
+  CONSTRAINT `employee_project_project_FK` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee_project`
+--
+
+LOCK TABLES `employee_project` WRITE;
+/*!40000 ALTER TABLE `employee_project` DISABLE KEYS */;
+INSERT INTO `employee_project` VALUES (1,1),(1,4),(2,8),(3,8),(3,9),(4,10),(3,10);
+/*!40000 ALTER TABLE `employee_project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `date_started` date NOT NULL,
+  `date_completion` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project`
+--
+
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+INSERT INTO `project` VALUES (1,'IDS','Integrated Disbursement System','2026-05-02',NULL),(2,'ECMS','Enhanced Collection Management System','2026-05-02',NULL),(3,'CCS','Collateral Claims System','2026-05-02',NULL),(4,'DRMS','Digital Records Management System','2026-05-02',NULL);
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'ciit project db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-02 16:50:14
